229eaceafbb9cb06d8f7549c60177d7ca0924e46
David Personette <dperson@gmail.com>
2015-05-16 14:23:56 -0400

    samba.sh fix behavior on config change when daemon is running

--- samba.sh
+++ samba.sh
@@ -104,7 +104,9 @@ shift $(( OPTIND - 1 ))
 
 [[ "${TIMEZONE:-""}" ]] && timezone "$TIMEZONE"
 
-if [[ $# -ge 1 && -x $(which $1 2>&-) ]]; then
+if ps -ef | grep -q smbd; then
+    echo "Service already running, please restart container to apply changes"
+elif [[ $# -ge 1 && -x $(which $1 2>&-) ]]; then
     exec "$@"
 elif [[ $# -ge 1 ]]; then
     echo "ERROR: command not found: $1"
