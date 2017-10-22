8ba7fd89a3e7824f4a37cd2eaed08d82f95f00aa
Sander Koenders <sanderkoenders@gmail.com>
2017-10-22 16:57:46 +0200

    Fix(group): groupmod the users group
    smbuser is assigned to the users group, change the id of the users group instead of the smbuser group.
    Resolves https://github.com/dperson/samba/issues/79

--- samba.sh
+++ samba.sh
@@ -193,7 +193,7 @@ The 'command' (if provided and valid) will be run instead of samba
 }
 
 [[ "${USERID:-""}" =~ ^[0-9]+$ ]] && usermod -u $USERID -o smbuser
-[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID -o smbuser
+[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID -o users
 
 while getopts ":hc:g:i:nprs:Su:Ww:" opt; do
     case "$opt" in
