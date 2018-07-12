be1ba62328131c168ca9e625172193def94cd8b8
Jon Dye <jon@pecorous.co.uk>
2018-07-12 09:23:43 +0100

    Add --no-process-group flag to smbd
    Latest Samba on Alpine fails with "Failed to create session" without
    this flag.

--- samba.sh
+++ samba.sh
@@ -242,5 +242,5 @@ elif ps -ef | egrep -v grep | grep -q smbd; then
     echo "Service already running, please restart container to apply changes"
 else
     [[ ${NMBD:-""} ]] && ionice -c 3 nmbd -D
-    exec ionice -c 3 smbd -FS </dev/null
+    exec ionice -c 3 smbd -FS --no-process-group </dev/null
 fi
