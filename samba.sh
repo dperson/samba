eb7e1530e574fc03a634d0d09ef8d68b40e4676d
Nick Miller <nick@nickmiller.me.uk>
2022-11-04 02:43:35 +0000

    Removed the -S flag from the smbd command which is no longer supported

--- samba.sh
+++ samba.sh
@@ -293,5 +293,5 @@ elif ps -ef | egrep -v grep | grep -q smbd; then
     echo "Service already running, please restart container to apply changes"
 else
     [[ ${NMBD:-""} ]] && ionice -c 3 nmbd -D
-    exec ionice -c 3 smbd -FS --no-process-group </dev/null
+    exec ionice -c 3 smbd -F --no-process-group </dev/null
 fi
